f93cdf79c0d067ba02908e8a7673bf4756394801
Iolanda Rinaldi <iolrin@MacBook-Pro-di-Iole.local>
2015-05-19 11:57:55 +0200

    aggiunto pannello per le opzioni utente

--- src/com/smartAPI/view/Panel_UserOptions.java
+++ src/com/smartAPI/view/Panel_UserOptions.java
@@ -5,6 +5,7 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.GridLayout;
+import java.awt.Color;
 
 public class Panel_UserOptions extends JPanel {
 
@@ -12,9 +13,10 @@ public class Panel_UserOptions extends JPanel {
 	 * Create the panel.
 	 */
 	public Panel_UserOptions() {
+		setBackground(new Color(72, 209, 204));
 		
 		JButton VisualizzaUtenti = new JButton("Visualizza Utenti");
-		VisualizzaUtenti.setBounds(300, 0, 144, 41);
+		VisualizzaUtenti.setBounds(387, 6, 123, 36);
 		VisualizzaUtenti.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
@@ -22,11 +24,11 @@ public class Panel_UserOptions extends JPanel {
 		setLayout(null);
 		
 		JButton Ricerca = new JButton("Ricerca");
-		Ricerca.setBounds(0, 0, 144, 41);
+		Ricerca.setBounds(17, 6, 123, 36);
 		add(Ricerca);
 		
 		JButton Inserisci = new JButton("Inserisci");
-		Inserisci.setBounds(150, 0, 144, 41);
+		Inserisci.setBounds(210, 6, 107, 36);
 		add(Inserisci);
 		add(VisualizzaUtenti);
 
