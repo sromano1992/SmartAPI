deb9c2b172b7dbb23d48a7bf9ef16275eb5153a8
Stefania <Stefania@pc-Stefania>
2015-05-20 09:33:38 +0200

    test Stefania

--- src/com/smartAPI/view/TestCiro.java
+++ src/com/smartAPI/view/TestCiro.java
@@ -9,10 +9,9 @@ public class TestCiro {
 	public static void main(String[] args) {
 		
 		//USERINFO
-		//UserInfoJPanel panel = new UserInfoJPanel("c.amati","123","Ciro","Amati","ciro.amati@gmail.com");
-		
+		UserInfoJPanel panel = new UserInfoJPanel("c.amati","123","Ciro","Amati","ciro.amati@gmail.com");
 		//CREATE NEW ACCOUNT
-		CreateNewAccountJPanel panel = new CreateNewAccountJPanel();
+		//CreateNewAccountJPanel panel = new CreateNewAccountJPanel();
 		JFrame frame= new JFrame();
 		frame.setVisible(true);
 		frame.setBounds(100, 100, 370, 537);
---
+++ src/com/smartAPI/view/Test_Ste.java
@@ -0,0 +1,24 @@
+package com.smartAPI.view;
+
+import java.awt.Color;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Test_Ste {
+
+	public static void main(String[] args) {
+				ShowUsers_Ste panel = new ShowUsers_Ste("s.carda","123","Stefania","Cardamone","s.cardamone@gmail.com");
+				//CREATE NEW ACCOUNT
+				//CreateNewAccountJPanel panel = new CreateNewAccountJPanel();
+				JFrame frame= new JFrame();
+				frame.setVisible(true);
+				frame.setBounds(100, 100, 370, 537);
+				frame.add(panel);
+
+	}
+
+}
+
+
+	
