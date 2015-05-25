f0189d020c1139d8e0fe3d9007854a501ac80190
Iolanda Rinaldi <iolrin@MBP-di-Iole.homenet.telecomitalia.it>
2015-05-25 18:18:30 +0200

    inserite visualizzazioni utente

--- src/com/smartAPI/view/ShowUsersInfo.java
+++ src/com/smartAPI/view/ShowUsersInfo.java
@@ -21,19 +21,25 @@ import com.smartAPI.model.Utente;
 
 public class ShowUsersInfo extends JPanel{
 
-	JLabel lblImage;
+	private JLabel lblImage;
 	private static int SCORE = 10;
 	private JTextField userField;
 	private JPasswordField passwordField;
 	private JTextField nameField;
 	private JTextField surnameField;
 	private JTextField emailField;
+	private JLabel lblEmail;
+	private JPanel panelStar;
+	private JLabel lblUser ;
+	private JLabel lblPassword;
+	private JLabel lblName;
+	private JLabel lblSurname;
 	private String avatar="";//iniz. con avatar user
 	
 	public ShowUsersInfo() {
 		setLayout(null);
 		setBackground(new Color(2, 94, 137));
-		lblImage = new JLabel("");
+		lblImage = new JLabel();
 		//path dipende dall'avatar dell'utente
 		String path= "res/3.png";
 		String pathIcon = getClass().getResource(path).getFile();
@@ -43,12 +49,12 @@ public class ShowUsersInfo extends JPanel{
 		lblImage.setBounds(49, 54, 80, 71);
 		add(lblImage);
 		
-		JPanel panelStar = new JPanel();
+		panelStar = new JPanel();
 		panelStar.setBackground(new Color(2, 94, 137));
 		panelStar.setBounds(154, 64, 127, 50);
 		add(panelStar);
 		
-		JLabel lblUser = new JLabel("User");
+		lblUser = new JLabel("User");
 		lblUser.setOpaque(true);
 		lblUser.setHorizontalAlignment(SwingConstants.CENTER);
 		lblUser.setForeground(Color.WHITE);
@@ -56,14 +62,14 @@ public class ShowUsersInfo extends JPanel{
 		lblUser.setBounds(18, 166, 95, 31);
 		add(lblUser);
 		
-		userField = new JTextField("---");
+		userField = new JTextField();
 		userField.setEditable(false);
 		userField.setColumns(10);
 		userField.setBackground(new Color(230, 230, 230));
 		userField.setBounds(110, 163, 204, 37);
 		add(userField);
 		
-		JLabel lblPassword = new JLabel("Password");
+		lblPassword = new JLabel("Password");
 		lblPassword.setOpaque(true);
 		lblPassword.setHorizontalAlignment(SwingConstants.CENTER);
 		lblPassword.setForeground(Color.WHITE);
@@ -71,13 +77,13 @@ public class ShowUsersInfo extends JPanel{
 		lblPassword.setBounds(18, 197, 95, 31);
 		add(lblPassword);
 		
-		passwordField = new JPasswordField("");
+		passwordField = new JPasswordField();
 		passwordField.setEditable(false);
 		passwordField.setBackground(new Color(230, 230, 230));
 		passwordField.setBounds(110, 193, 204, 37);
 		add(passwordField);
 		
-		JLabel lblName = new JLabel("Name");
+		lblName = new JLabel("Name");
 		lblName.setOpaque(true);
 		lblName.setHorizontalAlignment(SwingConstants.CENTER);
 		lblName.setForeground(Color.WHITE);
@@ -85,7 +91,7 @@ public class ShowUsersInfo extends JPanel{
 		lblName.setBounds(18, 258, 95, 31);
 		add(lblName);
 		
-		nameField = new JTextField("---");
+		nameField = new JTextField();
 		nameField.setEditable(false);
 		nameField.setBackground(new Color(230, 230, 230));
 		nameField.setColumns(10);
@@ -103,7 +109,7 @@ public class ShowUsersInfo extends JPanel{
 		});
 		add(nameField);
 		
-		JLabel lblSurname = new JLabel("Surname");
+		lblSurname = new JLabel("Surname");
 		lblSurname.setOpaque(true);
 		lblSurname.setHorizontalAlignment(SwingConstants.CENTER);
 		lblSurname.setForeground(Color.WHITE);
@@ -111,7 +117,7 @@ public class ShowUsersInfo extends JPanel{
 		lblSurname.setBounds(18, 288, 95, 31);
 		add(lblSurname);
 		
-		surnameField = new JTextField("---");
+		surnameField = new JTextField();
 		surnameField.addFocusListener(new FocusListener() {
 			
 			public void focusLost(FocusEvent e) {
@@ -128,7 +134,7 @@ public class ShowUsersInfo extends JPanel{
 		surnameField.setBounds(110, 284, 204, 37);
 		add(surnameField);
 		
-		JLabel lblEmail = new JLabel("Email");
+		lblEmail = new JLabel("Email");
 		lblEmail.setOpaque(true);
 		lblEmail.setHorizontalAlignment(SwingConstants.CENTER);
 		lblEmail.setForeground(Color.WHITE);
@@ -136,7 +142,7 @@ public class ShowUsersInfo extends JPanel{
 		lblEmail.setBounds(18, 318, 95, 31);
 		add(lblEmail);
 		
-		emailField = new JTextField("---");
+		emailField = new JTextField();
 		emailField.addFocusListener(new FocusListener() {
 			
 			public void focusLost(FocusEvent e) {
@@ -152,183 +158,183 @@ public class ShowUsersInfo extends JPanel{
 		emailField.setBackground(new Color(230, 230, 230));
 		emailField.setBounds(110, 314, 204, 37);
 		add(emailField);
-		path="res/pencil.png";
-		pathIcon = getClass().getResource(path).getFile();
-		imgicon = new MyImageIcon(pathIcon,20,20);
+//		path="res/pencil.png";
+//		pathIcon = getClass().getResource(path).getFile();
+//		imgicon = new MyImageIcon(pathIcon,20,20);
 		
-		Vector comboBoxItems=new Vector();
-	    comboBoxItems.add("avatar - 1");
-	    comboBoxItems.add("avatar - 2");
-	    comboBoxItems.add("avatar - 3");
-	    comboBoxItems.add("avatar - 4");
-	    comboBoxItems.add("avatar - 5");
-	    comboBoxItems.add("avatar - 6");
-	    final DefaultComboBoxModel model = new DefaultComboBoxModel(comboBoxItems);
-		path="res/pencil.png";
-		pathIcon = getClass().getResource(path).getFile();
-		imgicon = new MyImageIcon(pathIcon,20,20);
-		path="res/pencil.png";
-		pathIcon = getClass().getResource(path).getFile();
-		imgicon = new MyImageIcon(pathIcon,20,20);
-		ImageIcon star=new ImageIcon();
-		path="res/star.png";
-		pathIcon = getClass().getResource(path).getFile();
-		imgicon = new MyImageIcon(pathIcon,20,20);
-		star=imgicon.getImageResponsive();
-		for (int i=0; i<SCORE; i++){
-			panelStar.add(new JLabel(star));
-	    }
+//		Vector comboBoxItems=new Vector();
+//	    comboBoxItems.add("avatar - 1");
+//	    comboBoxItems.add("avatar - 2");
+//	    comboBoxItems.add("avatar - 3");
+//	    comboBoxItems.add("avatar - 4");
+//	    comboBoxItems.add("avatar - 5");
+//	    comboBoxItems.add("avatar - 6");
+//	    final DefaultComboBoxModel model = new DefaultComboBoxModel(comboBoxItems);
+//		path="res/pencil.png";
+//		pathIcon = getClass().getResource(path).getFile();
+//		imgicon = new MyImageIcon(pathIcon,20,20);
+//		path="res/pencil.png";
+//		pathIcon = getClass().getResource(path).getFile();
+//		imgicon = new MyImageIcon(pathIcon,20,20);
+//		ImageIcon star=new ImageIcon();
+//		path="res/star.png";
+//		pathIcon = getClass().getResource(path).getFile();
+//		imgicon = new MyImageIcon(pathIcon,20,20);
+//		star=imgicon.getImageResponsive();
+//		for (int i=0; i<SCORE; i++){
+//			panelStar.add(new JLabel(star));
+//	    }
 		
 	}
 			
 	public void setUser(Utente u) {
-				setLayout(null);
-				setBackground(new Color(2, 94, 137));
-				lblImage = new JLabel("");
+				//setLayout(null);
+				//setBackground(new Color(2, 94, 137));
+				//lblImage = new JLabel("");
 				//path dipende dall'avatar dell'utente
 				String path=u.getAvatar();
 				String pathIcon = getClass().getResource(path).getFile();
 				MyImageIcon imgicon = new MyImageIcon(pathIcon,80,70);
 				lblImage.setIcon(imgicon.getImageResponsive());
 				
-				lblImage.setBounds(49, 54, 80, 71);
-				add(lblImage);
+				//lblImage.setBounds(49, 54, 80, 71);
+				//add(lblImage);
 				
-				JPanel panelStar = new JPanel();
-				panelStar.setBackground(new Color(2, 94, 137));
-				panelStar.setBounds(154, 64, 127, 50);
-				add(panelStar);
+				//JPanel panelStar = new JPanel();
+				//panelStar.setBackground(new Color(2, 94, 137));
+				//panelStar.setBounds(154, 64, 127, 50);
+				//add(panelStar);
 				
-				JLabel lblUser = new JLabel("User");
-				lblUser.setOpaque(true);
-				lblUser.setHorizontalAlignment(SwingConstants.CENTER);
-				lblUser.setForeground(Color.WHITE);
-				lblUser.setBackground(new Color(2, 66, 96));
-				lblUser.setBounds(18, 166, 95, 31);
-				add(lblUser);
+				//JLabel lblUser = new JLabel("User");
+				//lblUser.setOpaque(true);
+				//lblUser.setHorizontalAlignment(SwingConstants.CENTER);
+				//lblUser.setForeground(Color.WHITE);
+				//lblUser.setBackground(new Color(2, 66, 96));
+				//lblUser.setBounds(18, 166, 95, 31);
+				//add(lblUser);
 				
-				userField = new JTextField(u.getNome());
-				userField.setEditable(false);
-				userField.setColumns(10);
-				userField.setBackground(new Color(230, 230, 230));
-				userField.setBounds(110, 163, 204, 37);
-				add(userField);
+				userField.setText(u.getNickname());
+//				userField.setEditable(false);
+//				userField.setColumns(10);
+//				userField.setBackground(new Color(230, 230, 230));
+//				userField.setBounds(110, 163, 204, 37);
+//				add(userField);
 				
-				JLabel lblPassword = new JLabel("Password");
-				lblPassword.setOpaque(true);
-				lblPassword.setHorizontalAlignment(SwingConstants.CENTER);
-				lblPassword.setForeground(Color.WHITE);
-				lblPassword.setBackground(new Color(2, 66, 96));
-				lblPassword.setBounds(18, 197, 95, 31);
-				add(lblPassword);
+//				JLabel lblPassword = new JLabel("Password");
+//				lblPassword.setOpaque(true);
+//				lblPassword.setHorizontalAlignment(SwingConstants.CENTER);
+//				lblPassword.setForeground(Color.WHITE);
+//				lblPassword.setBackground(new Color(2, 66, 96));
+//				lblPassword.setBounds(18, 197, 95, 31);
+//				add(lblPassword);
 				
-				passwordField = new JPasswordField("");
-				passwordField.setEditable(false);
-				passwordField.setBackground(new Color(230, 230, 230));
-				passwordField.setBounds(110, 193, 204, 37);
-				add(passwordField);
+				passwordField.setText(u.getPassword());
+//				passwordField.setEditable(false);
+//				passwordField.setBackground(new Color(230, 230, 230));
+//				passwordField.setBounds(110, 193, 204, 37);
+//				add(passwordField);
 				
-				JLabel lblName = new JLabel("Name");
-				lblName.setOpaque(true);
-				lblName.setHorizontalAlignment(SwingConstants.CENTER);
-				lblName.setForeground(Color.WHITE);
-				lblName.setBackground(new Color(2, 66, 96));
-				lblName.setBounds(18, 258, 95, 31);
-				add(lblName);
+//				JLabel lblName = new JLabel("Name");
+//				lblName.setOpaque(true);
+//				lblName.setHorizontalAlignment(SwingConstants.CENTER);
+//				lblName.setForeground(Color.WHITE);
+//				lblName.setBackground(new Color(2, 66, 96));
+//				lblName.setBounds(18, 258, 95, 31);
+//				add(lblName);
 				
-				nameField = new JTextField(u.getNome());
-				nameField.setEditable(false);
-				nameField.setBackground(new Color(230, 230, 230));
-				nameField.setColumns(10);
-				nameField.setBounds(110, 258, 204, 37);
-				nameField.addFocusListener(new FocusListener() {
-					public void focusLost(FocusEvent e) {
-						nameField.setBackground(Color.WHITE);
-						
-					}
-					
-					public void focusGained(FocusEvent e) {
-						// TODO Auto-generated method stub
-						
-					}
-				});
-				add(nameField);
+				nameField.setText(u.getNome());
+//				nameField.setEditable(false);
+//				nameField.setBackground(new Color(230, 230, 230));
+//				nameField.setColumns(10);
+//				nameField.setBounds(110, 258, 204, 37);
+//				nameField.addFocusListener(new FocusListener() {
+//					public void focusLost(FocusEvent e) {
+//						nameField.setBackground(Color.WHITE);
+//						
+//					}
+//					
+//					public void focusGained(FocusEvent e) {
+//						// TODO Auto-generated method stub
+//						
+//					}
+//				});
+//				add(nameField);
 				
-				JLabel lblSurname = new JLabel("Surname");
-				lblSurname.setOpaque(true);
-				lblSurname.setHorizontalAlignment(SwingConstants.CENTER);
-				lblSurname.setForeground(Color.WHITE);
-				lblSurname.setBackground(new Color(2, 66, 96));
-				lblSurname.setBounds(18, 288, 95, 31);
-				add(lblSurname);
+//				JLabel lblSurname = new JLabel("Surname");
+//				lblSurname.setOpaque(true);
+//				lblSurname.setHorizontalAlignment(SwingConstants.CENTER);
+//				lblSurname.setForeground(Color.WHITE);
+//				lblSurname.setBackground(new Color(2, 66, 96));
+//				lblSurname.setBounds(18, 288, 95, 31);
+//				add(lblSurname);
 				
-				surnameField = new JTextField(u.getCognome());
-				surnameField.addFocusListener(new FocusListener() {
-					
-					public void focusLost(FocusEvent e) {
-						surnameField.setBackground(Color.WHITE);
-					}
-					
-					public void focusGained(FocusEvent e) {
-						
-					}
-				});
+				surnameField.setText(u.getCognome());
+//				surnameField.addFocusListener(new FocusListener() {
+//					
+//					public void focusLost(FocusEvent e) {
+//						surnameField.setBackground(Color.WHITE);
+//					}
+//					
+//					public void focusGained(FocusEvent e) {
+//						
+//					}
+//				});
+//				
+//				surnameField.setEditable(false);
+//				surnameField.setBackground(new Color(230, 230, 230));
+//				surnameField.setBounds(110, 284, 204, 37);
+//				add(surnameField);
 				
-				surnameField.setEditable(false);
-				surnameField.setBackground(new Color(230, 230, 230));
-				surnameField.setBounds(110, 284, 204, 37);
-				add(surnameField);
+//				JLabel lblEmail = new JLabel("Email");
+//				lblEmail.setOpaque(true);
+//				lblEmail.setHorizontalAlignment(SwingConstants.CENTER);
+//				lblEmail.setForeground(Color.WHITE);
+//				lblEmail.setBackground(new Color(2, 66, 96));
+//				lblEmail.setBounds(18, 318, 95, 31);
+//				add(lblEmail);
 				
-				JLabel lblEmail = new JLabel("Email");
-				lblEmail.setOpaque(true);
-				lblEmail.setHorizontalAlignment(SwingConstants.CENTER);
-				lblEmail.setForeground(Color.WHITE);
-				lblEmail.setBackground(new Color(2, 66, 96));
-				lblEmail.setBounds(18, 318, 95, 31);
-				add(lblEmail);
+				emailField.setText(u.getEmail());
+//				emailField.addFocusListener(new FocusListener() {
+//					
+//					public void focusLost(FocusEvent e) {
+//						emailField.setBackground(Color.WHITE);
+//						
+//					}
+//					public void focusGained(FocusEvent e) {
+//						// TODO Auto-generated method stub
+//						
+//					}
+//				});
+//				emailField.setEditable(false);
+//				emailField.setBackground(new Color(230, 230, 230));
+//				emailField.setBounds(110, 314, 204, 37);
+//				add(emailField);
+//				path="res/pencil.png";
+//				pathIcon = getClass().getResource(path).getFile();
+//				imgicon = new MyImageIcon(pathIcon,20,20);
 				
-				emailField = new JTextField(u.getEmail());
-				emailField.addFocusListener(new FocusListener() {
-					
-					public void focusLost(FocusEvent e) {
-						emailField.setBackground(Color.WHITE);
-						
-					}
-					public void focusGained(FocusEvent e) {
-						// TODO Auto-generated method stub
-						
-					}
-				});
-				emailField.setEditable(false);
-				emailField.setBackground(new Color(230, 230, 230));
-				emailField.setBounds(110, 314, 204, 37);
-				add(emailField);
-				path="res/pencil.png";
-				pathIcon = getClass().getResource(path).getFile();
-				imgicon = new MyImageIcon(pathIcon,20,20);
-				
-				Vector comboBoxItems=new Vector();
-			    comboBoxItems.add("avatar - 1");
-			    comboBoxItems.add("avatar - 2");
-			    comboBoxItems.add("avatar - 3");
-			    comboBoxItems.add("avatar - 4");
-			    comboBoxItems.add("avatar - 5");
-			    comboBoxItems.add("avatar - 6");
-			    final DefaultComboBoxModel model = new DefaultComboBoxModel(comboBoxItems);
-				path="res/pencil.png";
-				pathIcon = getClass().getResource(path).getFile();
-				imgicon = new MyImageIcon(pathIcon,20,20);
-				path="res/pencil.png";
-				pathIcon = getClass().getResource(path).getFile();
-				imgicon = new MyImageIcon(pathIcon,20,20);
-				ImageIcon star=new ImageIcon();
-				path="res/star.png";
-				pathIcon = getClass().getResource(path).getFile();
-				imgicon = new MyImageIcon(pathIcon,20,20);
-				star=imgicon.getImageResponsive();
-				for (int i=0; i<SCORE; i++){
-					panelStar.add(new JLabel(star));
-			    }
+//				Vector comboBoxItems=new Vector();
+//			    comboBoxItems.add("avatar - 1");
+//			    comboBoxItems.add("avatar - 2");
+//			    comboBoxItems.add("avatar - 3");
+//			    comboBoxItems.add("avatar - 4");
+//			    comboBoxItems.add("avatar - 5");
+//			    comboBoxItems.add("avatar - 6");
+//			    final DefaultComboBoxModel model = new DefaultComboBoxModel(comboBoxItems);
+//				path="res/pencil.png";
+//				pathIcon = getClass().getResource(path).getFile();
+//				imgicon = new MyImageIcon(pathIcon,20,20);
+//				path="res/pencil.png";
+//				pathIcon = getClass().getResource(path).getFile();
+//				imgicon = new MyImageIcon(pathIcon,20,20);
+//				ImageIcon star=new ImageIcon();
+//				path="res/star.png";
+//				pathIcon = getClass().getResource(path).getFile();
+//				imgicon = new MyImageIcon(pathIcon,20,20);
+//				star=imgicon.getImageResponsive();
+//				for (int i=0; i<SCORE; i++){
+//					panelStar.add(new JLabel(star));
+//			    }
 				
 			}
 
