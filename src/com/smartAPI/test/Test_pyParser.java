b9464d5bd6e2476e289c7dcd2269ddce9dded109
s.romano1992 <s.romano1992@gmail.com>
2015-05-25 11:20:53 +0200

    up

--- src/com/smartAPI/test/Test_pyParser.java
+++ src/com/smartAPI/test/Test_pyParser.java
@@ -58,13 +58,13 @@ public class Test_pyParser {
 
 	}
 	
-	public void runWith_jython(){
+	/*public void runWith_jython(){
 		PythonInterpreter interpreter = new PythonInterpreter();
 		interpreter.exec("import sys\nsys.path.append('pathToModiles if they're not there by default')\nimport yourModule");
 		// execute a function that takes a string and returns a string
 		PyObject someFunc = interpreter.get("funcName");
 		PyObject result = someFunc.__call__(new PyString("Test!"));
 		String realResult = (String) result.__tojava__(String.class);
-	}
+	}*/
 
 }
