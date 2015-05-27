e6c22659f04bc7a92e4c300a4aab4d5084ee067b
s.romano1992 <s.romano1992@gmail.com>
2015-05-27 17:20:33 +0200

    Updated '.owl'

--- src/com/smartAPI/test/Test_other_simone.java
+++ src/com/smartAPI/test/Test_other_simone.java
@@ -6,8 +6,13 @@ public class Test_other_simone {
 
 	public static void main(String[] args) {
 		SmartAPIModel s = new SmartAPIModel();
-		System.out.println(s.getPatternOfCategory("Storage").getBasicCodePattern());
-		System.out.println(s.getPatternOfCategory("Storage").getInferredCodePattern());
+		//System.out.println(s.getPatternOfCategory("Storage").getBasicCodePattern());
+		//System.out.println(s.getPatternOfCategory("Storage").getInferredCodePattern());
+
+		s.associateCatAndCodePattern("Storage", "s6");
+		//s.addKeyword("s6", "keyword");
+		s.setCPSourceCode("println", "s6");
+		s.storeOntModel();
 	}
 
 }
