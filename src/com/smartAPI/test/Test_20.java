2ddd53b679ac4be69033bef0f8b11f0417ec34d5
Iolanda Rinaldi <iolrin@MacBook-Pro-di-Iole.local>
2015-05-28 17:40:12 +0200

    modifica insert cp

--- src/com/smartAPI/test/Test_20.java
+++ src/com/smartAPI/test/Test_20.java
@@ -5,6 +5,7 @@ import japa.parser.ast.CompilationUnit;
 import japa.parser.ast.body.FieldDeclaration;
 import japa.parser.ast.body.MethodDeclaration;
 import japa.parser.ast.body.VariableDeclarator;
+import japa.parser.ast.body.VariableDeclaratorId;
 import japa.parser.ast.expr.Expression;
 import japa.parser.ast.expr.FieldAccessExpr;
 import japa.parser.ast.expr.MethodCallExpr;
@@ -34,9 +35,8 @@ public class Test_20 {
 	public static Logger logger = Logger.getLogger("global");
 	public static String CLASS_DEF = "public class Prova{ ";
     public static void main(String[] args) throws Exception {
-    	String code = "	public static JFrame f; public static void main(String[] args) {f = new JFrame(); f.setSize(1300, 900);f.add(new Desktop_0(f));f.setVisible(true);f.setDefaultCloseOperation(f.EXIT_ON_CLOSE);}";
-    	
-    	
+    	//String code = "	public JFrame f; public String stringa; public  void metodo() {String f=null; f.toString(); f.setSize(1300, 900);f.add(new Desktop_0(f));f.setVisible(true);f.setDefaultCloseOperation(f.EXIT_ON_CLOSE);} public static void main(String[]args){f.reload(); stringa.stampa();} ";
+    	String code="public void metodo() { VariableVisitor1 vis1 = new VariableVisitor1(); vis1.visit(cu, null);}";
     /*	String code = "public class Prova{private static File createFile(String testo) throws "+ "IOException{String content = testo; File file = new File(\"test1.java\");if (!file.exists())"
     			+ "{file.createNewFile();}FileWriter fw = new FileWriter(file.getAbsoluteFile()); "
     			+ "BufferedWriter bw = new BufferedWriter(fw);bw.write(content);bw.close(); return file;}}";*/
@@ -55,16 +55,20 @@ public class Test_20 {
         }
         
         
+        VariableVisitor1 vis1 = new VariableVisitor1();
+        vis1.visit(cu, null);
+        ArrayList<FieldDeclaration> variab= vis1.getVarList();
+        System.out.println("field: "+variab);
+        
         VariableVisitor varVisitor = new VariableVisitor();
         varVisitor.visit(cu, null);
         ArrayList<VariableDeclarationExpr> variables = varVisitor.getVarList();
-       System.out.println("variables "+variables);
+       System.out.println("variables: "+variables);
         MethodVisitor methVisitor = new MethodVisitor();
         methVisitor.visit(cu, null);
         ArrayList<MethodCallExpr> methods = methVisitor.getMethodList();
-       
-	       System.out.println(variables);
-	       System.out.println(methods);
+
+	       System.out.println("methods: "+methods);
 	       HashMap<String,String> toReturn = new HashMap<String, String>();
 	       
 	      
@@ -80,14 +84,26 @@ public class Test_20 {
 	    	    }
 	    	   }
 	       }
+	       
+	       
 	       ArrayList<String> method_s = new ArrayList<String>();
 	       method_s.addAll(toReturn.keySet());
 	       System.out.println(method_s);
     }
 
 
-    private static class VariableVisitor extends VoidVisitorAdapter {
+    private static class VariableVisitor1 extends VoidVisitorAdapter {
+     	ArrayList<FieldDeclaration> variables1 = new ArrayList<FieldDeclaration>();
+        public void visit(FieldDeclaration n, Object arg) {
+        	variables1.add(n);
+        }
         
+        public ArrayList<FieldDeclaration> getVarList(){
+        	return variables1; 
+        }
+    }
+    
+    private static class VariableVisitor extends VoidVisitorAdapter {
      	ArrayList<VariableDeclarationExpr> variables = new ArrayList<VariableDeclarationExpr>();
         public void visit(VariableDeclarationExpr n, Object arg) {
         	variables.add(n);
