574f6d9df22d69343146bfafb7cf47f78a337599
s.romano1992 <s.romano1992@gmail.com>
2015-06-06 12:09:13 +0200

    Added icon to main frame

--- src/com/smartAPI/test/Main.java
+++ src/com/smartAPI/test/Main.java
@@ -9,6 +9,7 @@ import com.smartAPI.model.SmartAPIModel;
 import com.smartAPI.view.CreateAdminJPanel;
 import com.smartAPI.view.Desktop_0;
 import java.awt.Toolkit;
+import java.awt.Window.Type;
 
 public class Main {
 	public static  JFrame f;
@@ -17,17 +18,19 @@ public class Main {
 	public static void main(String[] args) {
 		SmartAPIModel m = new SmartAPIModel();
 		f = new JFrame();
+		f.setTitle("SmartAPI 1.0");
+		f.setIconImage(Toolkit.getDefaultToolkit().getImage(Main.class.getResource("/com/smartAPI/view/res/basic-code-icon.png")));
 		fAdmin = new JFrame();
 		f.setSize(1300, 900);
 		fAdmin.setSize(1300, 900);
 
 		if (m.createAdmin()){
-			fAdmin.add(new CreateAdminJPanel());
+			fAdmin.getContentPane().add(new CreateAdminJPanel());
 			fAdmin.setVisible(true);
 			fAdmin.setDefaultCloseOperation(f.EXIT_ON_CLOSE);
 		}
 		else{
-			f.add(new Desktop_0(f));
+			f.getContentPane().add(new Desktop_0(f));
 			f.setVisible(true);
 			f.setDefaultCloseOperation(f.EXIT_ON_CLOSE);
 		}
@@ -47,7 +50,7 @@ public class Main {
 	
 	public static void setVisible() {
 		f.setVisible(true);
-		f.add(new Desktop_0(f));
+		f.getContentPane().add(new Desktop_0(f));
 		f.setDefaultCloseOperation(f.EXIT_ON_CLOSE);
 	}
 }
